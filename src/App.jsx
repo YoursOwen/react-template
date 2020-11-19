503241363ef7c77e010cc6aec5e4a54e0b8d5591
Owen Min <minyuwen@126.com>
2020-11-19 15:11:17 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useDebugValue } from 'react'
+import React, { Suspense } from 'react'
 import { HashRouter as Router } from 'react-router-dom'
 import Routes from './router'
 
